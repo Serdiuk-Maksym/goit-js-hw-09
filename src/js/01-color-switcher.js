d846599c4310dd984afcd7c83bfea89f0be6c273
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-05-28 20:11:05 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let intervalId;
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    document.body.classList.add('active');
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  document.body.classList.remove('active');
+});
